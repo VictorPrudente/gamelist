9a9a9dacc496ca09e61a316d229a677a60bc0770
Victor Prudente <victorprudt@gmail.com>
2023-11-18 18:46:50 -0300

    Move games and update on Database

--- src/main/java/com/prudente/gamelist/services/GameListService.java
+++ src/main/java/com/prudente/gamelist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.prudente.gamelist.services;
 
 import com.prudente.gamelist.dto.GameListDTO;
 import com.prudente.gamelist.entities.GameList;
+import com.prudente.gamelist.projections.GameMinProjection;
 import com.prudente.gamelist.repositories.GameListRepository;
+import com.prudente.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +17,29 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(gameList -> new GameListDTO(gameList)).toList();
     }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection game = list.remove(sourceIndex);
+        list.add(destinationIndex, game);
+
+        int min = Math.min(sourceIndex, destinationIndex);
+        int max = Math.max(sourceIndex, destinationIndex);
+
+        for (int i = min; i <= max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
+    }
+
 
 }
